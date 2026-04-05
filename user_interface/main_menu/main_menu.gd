4ef54bab717f92706fc08407d1b70b5e99d76508
alexkef41 <alexkefalidis41@gmail.com>
2026-04-06 02:01:59 +0300

    added basic ui for heart system/ audio

--- user_interface/main_menu/main_menu.gd
+++ user_interface/main_menu/main_menu.gd
@@ -1,26 +1,22 @@
 extends Control
 
 @onready var menu_buttons: VBoxContainer = $MenuButtons
-@onready var options: Panel = $Options
+@onready var options_menu: Control = $OptionsMenu
+
 
 @export var starting_scene: String
 
 func _ready() -> void:
 	menu_buttons.visible = true
-	options.visible = false
+	options_menu.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file(starting_scene)
 
 
 func _on_options_pressed() -> void:
-	menu_buttons.visible = false
-	options.visible = true
+	options_menu.open(menu_buttons)
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_back_pressed() -> void:
-	_ready()
