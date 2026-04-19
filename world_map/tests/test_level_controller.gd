5f9270b7148ea49998a50c91d2bde960afa338a4
Konnosdik <konnosdi@gmail.com>
2026-04-19 17:19:45 +0300

    Parallax added
    Parallax background added and configured. A line of script was also added in the level controller to find the sun in the player and play the animation

--- world_map/tests/test_level_controller.gd
+++ world_map/tests/test_level_controller.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @onready var player_character: CharacterBody2D = $test_character
 @onready var map_generator: MapGenerator = $MapGenerator
-@onready var start_fragment: MapFragment = $snow1
+@onready var start_fragment: MapFragment = $snow1       
+@onready var sun_anim: AnimatedSprite2D = player_character.get_node_or_null("sun") as AnimatedSprite2D                  
 
 func _ready() -> void:
 	@warning_ignore("unsafe_method_access") # it is not unsafe...
 	player_character.c_setup_character(map_generator,start_fragment)
+	sun_anim.play()
